5900b50b1124dc9e19f08f32abbe94c5f15d27c3
chenyun <chenyun@cai-inc.com>
2018-08-17 17:49:12 +0800

    添加操作指南&&反爬虫

--- src/main/java/datacrawler/HtmlLoad.java
+++ src/main/java/datacrawler/HtmlLoad.java
@@ -9,6 +9,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created by cy111966 on 2016/11/15.
@@ -17,12 +18,13 @@ public class HtmlLoad {
   static Logger log = LoggerFactory.getLogger("logfile");
   static Logger log_error = LoggerFactory.getLogger("errorfile");
 public static final String dir ="/Users/chenyun/stockData/holders";
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     String[] codes = Constant.stock_all.split(",");
     String path="http://basic.10jqka.com.cn/32/${code}/holder.html";
     for (String code : codes) {
       String url = path.replace("${code}",code);
       System.out.println(url);
+      Thread.sleep(new Random().nextInt(500));
       load(url,dir,code);
     }
   }
@@ -36,7 +38,7 @@ public static final String dir ="/Users/chenyun/stockData/holders";
    */
   public static void load(String url,String outpath,String name){
     try {
-      Document doc = Jsoup.connect(url).timeout(10000).get();
+      Document doc = Jsoup.connect(url).timeout(10000).userAgent("Mozilla").get();
       System.out.println(doc.html());
       String fullName = outpath+"/"+name+".html";
       System.out.println(fullName);
