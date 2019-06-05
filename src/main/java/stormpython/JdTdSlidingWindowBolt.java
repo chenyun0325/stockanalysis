415138d73272089a05c49c9309deb9e55c8ebe51
chenyun <chenyun@cai-inc.com>
2019-06-05 11:19:34 +0800

    数据防止重复

--- src/main/java/stormpython/JdTdSlidingWindowBolt.java
+++ src/main/java/stormpython/JdTdSlidingWindowBolt.java
@@ -14,7 +14,6 @@ import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Fields;
-import org.apache.storm.tuple.MessageId;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 import org.slf4j.Logger;
